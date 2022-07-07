9017b03c5b478e3df0637739161a6083d12e935e
Crowdbotics <team@crowdbotics.com>
2022-07-07 09:15:46 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "oa_currency_manager_36072",
+  url: "https://oa_currency_manager_36072.botics.co",
+  api: "https://oa_currency_manager_36072.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
